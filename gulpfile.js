93a1b08b8298293a2bc1392b56946daaf2ca0b10
Noris <noris75564@gmail.com>
2018-04-21 09:34:56 +0300

    info

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,13 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const preproc = require('gulp-less');
-const gcmq = require('gulp-group-css-media-queries');
-const autoprefixer = require('gulp-autoprefixer');
-const cleanCSS = require('gulp-clean-css');
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
-const cssmin = require('gulp-cssmin');
-const rename = require('gulp-rename');
-const htmlmin = require('gulp-htmlmin');
-const ejs = require('gulp-ejs');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+var htmlmin = require('gulp-htmlmin');
+var ejs = require('gulp-ejs');
 // var babel = require('gulp-babel');
-const fs = require('fs');
+var fs = require('fs');
 
 gulp.task('grid', function () {
     return gulp.src('src/style/**/grid-system.scss')
@@ -26,19 +22,6 @@ gulp.task('sass', function () {
         .pipe(cssmin())
         .pipe(gulp.dest('dist/style/'));
 });
-gulp.task('preproc', function () {
-    return gulp.src('src/precss/styles.less')
-        .pipe(preproc())
-        .pipe(gcmq())
-        .pipe(autoprefixer({
-            browsers: ['> 0.1%'],
-            cascade: false
-        }))
-        .pipe(cleanCSS({
-            level: 2
-        }))
-        .pipe(gulp.dest('dist/style/'));
-});
 
 gulp.task('scripts', function () {
     return gulp.src('src/js/**/*.js')
@@ -48,21 +31,21 @@ gulp.task('scripts', function () {
         .pipe(uglify().on('error', function (e) {
             console.log(e);
         }))
-        .pipe(gulp.dest('./dist/js/'))
+        .pipe(gulp.dest('./dist/'))
 });
 
-gulp.task('sections', ['preproc'], function () {
-    var criticalStyle = fs.readFileSync('./dist/style/styles.css', 'utf8');
+gulp.task('sections', ['sass'], function () {
+    var criticalStyle = fs.readFileSync('./dist/style/index.css', 'utf8');
     var version = '4';
-    return gulp.src('./src/*.html')
-        // .pipe(ejs({criticalStyle: criticalStyle, version: version}, {}, {ext: '.html'}))
+    return gulp.src('./src/*.ejs')
+        .pipe(ejs({criticalStyle: criticalStyle, version: version}, {}, {ext: '.html'}))
         .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('./dist'))
 });
 
 gulp.task('assets', function () {
-    gulp.src(['./src/img/**/*'])
-        .pipe(gulp.dest('./dist/img'))
+    gulp.src(['./src/images/**/*'])
+        .pipe(gulp.dest('./dist/images'))
 });
 
 gulp.task('fonts', function () {
@@ -74,9 +57,8 @@ gulp.task('watch', ['default'], function () {
     gulp.watch('src/js/**/*.js', ['scripts']);
     gulp.watch('src/**/*.ejs', ['sections']);
     gulp.watch('src/style/**/*.scss', ['sass', 'sections']);
-    gulp.watch('src/style/**/*.less', ['preproc']);
-    gulp.watch('src/img/**/*', ['assets']);
+    gulp.watch('src/images/**/*', ['assets']);
 });
 
 
-gulp.task('default', ['sass','preproc', 'assets', 'scripts', 'sections', 'fonts', 'grid']);
+gulp.task('default', ['sass', 'assets', 'scripts', 'sections', 'fonts', 'grid']);
